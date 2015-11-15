9d35083e9b4a428f911732c12289a8cddeed449b
Jiang Bian <bj911125@gmail.com>
2015-11-15 18:20:18 -0500

    Optimization.vb created

---
+++ HomeBuilder/App_Code/Optimization.vb
@@ -0,0 +1,47 @@
+﻿Imports Microsoft.VisualBasic
+Imports Microsoft.SolverFoundation.Services
+
+Public Class TeamOptimizer
+    ' inputs
+
+    ' model
+
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub LoadData(ByVal aDataLoader As DataLoader)
+
+    End Sub
+
+    Public Sub Solve()
+        ' creates a Solver and a Model
+        Dim mySolverContext As SolverContext = SolverContext.GetContext
+        myModel = mySolverContext.CreateModel
+        ' Decision variables
+        AddDecisions()
+        ' Constraints
+        AddConstraints()
+        ' Objective Function
+        AddGoal()
+        ' Solve
+        mySolverContext.Solve()   ' or specifically    mySolverContext.Solve(New SimplexDirective)
+    End Sub
+
+    Private Sub AddDecisions()
+
+    End Sub
+
+    Private Sub AddConstraints()
+
+    End Sub
+
+    Private Sub AddGoal()
+
+    End Sub
+
+    Public Function Results() As OptimizationResults
+        Return New OptimizationResults
+    End Function
+End Class
