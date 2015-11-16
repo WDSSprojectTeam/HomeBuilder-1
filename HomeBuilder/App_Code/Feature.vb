5043b57ef4c31a98ac02a4dbb88196c94dd0439f
Kathryn Massengill <kmassengill@ufl.edu>
2015-11-16 15:57:23 -0500

    dataloader, feature, options, room classes added

---
+++ HomeBuilder/App_Code/Feature.vb
@@ -0,0 +1,24 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class Feature
+    Private FeatureList As New List(Of Options)
+
+    Public Sub Addtofeaturelist(ByVal anoption As Options)
+        FeatureList.Add(anoption)
+    End Sub
+
+    Public Function Getfeaturelist() As ArrayList
+
+        Dim allfeatures As New ArrayList
+        Dim anoption As Options
+        For Each anoption In FeatureList
+            If (Not allfeatures.Contains(anoption.getoptionfeature)) Then
+                allfeatures.Add(anoption.getoptionfeature)
+            End If
+        Next
+        Return allfeatures
+
+    End Function
+
+
+End Class
---
+++ HomeBuilder/App_Code/Options.vb
@@ -0,0 +1,58 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class Options
+    Private ID As Integer
+    Private Name As String
+    Private Price As Double
+    Private Description As String
+    Private Feature As Integer
+
+    Public Sub New(ByVal optid As Integer, ByVal optname As String, ByVal optprice As Double, ByVal optdescription As String, ByVal optfeature As String)
+
+        ID = optid
+        Name = optname
+        Price = optprice
+        Description = optdescription
+        Feature = optfeature
+
+    End Sub
+
+    Public ReadOnly Property getoptionID As Integer
+        Get
+            Return ID
+        End Get
+    End Property
+
+    Public ReadOnly Property getoptionname As String
+        Get
+            Return Name
+        End Get
+    End Property
+
+    Public ReadOnly Property getoptionprice As Double
+        Get
+            Return Price
+        End Get
+    End Property
+
+    Public Function CalculateTotalPrice(ByVal squarefeet As Double) As Double
+        Dim totalprice As Double
+
+
+
+        Return totalprice
+    End Function
+
+    Public ReadOnly Property getoptiondescription As String
+        Get
+            Return Description
+        End Get
+    End Property
+
+    Public ReadOnly Property getoptionfeature As String
+        Get
+            Return Feature
+        End Get
+    End Property
+
+End Class
